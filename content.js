e382f01d0addcf2413e85aa407be4b26a24cbaf5
Andrey Latayko <anrey.latayko@dataart.com>
2015-07-13 23:05:38 +0200

    add AngularJS for ticket template processing

--- content.js
+++ content.js
@@ -1,18 +1,27 @@
+'use strict';
+
+
+var newTicket = $("<div ng-controller='Ctrl'>ticket QR Code: <img ng-src='{{QR}}'/></div>").addClass("tkNew"); 
+window.name = '';
+angular.bootstrap(newTicket, ['app'])
+             
+       
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.action == "GetBody") {
+     if (request.action == "GetBody") {
       
 	   var oldTicket = $(".tkOld");
      if (oldTicket.size() == 0) {
         oldTicket = $('.blank.electronic').addClass("tkOld");
       }
      
-     var newTicket = $(".tkNew");
-     if (newTicket.size() == 0) { 
-       newTicket = $("<div></div>").addClass("tkNew");
-       newTicket.append(oldTicket.find('img').clone());       
+     if (newTicket.parent().size() == 0) {
+       var scope = angular.element(newTicket).scope();
+       console.log(scope);
+       scope.$apply(function(){scope.QR = oldTicket.find('img').clone().attr('src')});
+       //newTicket.append(oldTicket.find('img').clone());       
        oldTicket.parent().append(newTicket);
-	    
      }
      
      
---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var myApp = angular.module("app", []);
+
+myApp.factory('Data', function(){
+    //return {message: "I'm data from a service"};
+});
+
+myApp.controller("Ctrl", function($scope, $http){
+
+    this.message = "This is test ticket";
+});
